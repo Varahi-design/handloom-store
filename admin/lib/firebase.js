dd8aa55d2d828c08fe3e9e04aa822d4e0b13af9a
Varahi-Design <2025varahi@gmail.com>
2026-05-03 17:59:47 +0530

    Use Firebase env vars for admin panel

--- admin/lib/firebase.js
+++ admin/lib/firebase.js
@@ -3,13 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBJaB8eqxYJAluDIcgTUsRJ5Win7L0nFgs",
-  authDomain: "handloom-store.firebaseapp.com",
-  projectId: "handloom-store",
-  storageBucket: "handloom-store.firebasestorage.app",
-  messagingSenderId: "113171757206",
-  appId: "1:113171757206:web:e58fb30ce7dc26eb646ab7",
-  measurementId: "G-FJYXS6VC87"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
